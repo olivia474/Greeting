38a31c7c1ecf1e494cb70936408b6b1facfbcca5
olivia474 <olivia.lengthorn@bp.com>
2023-09-29 10:28:54 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Greeting;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("What is your name?");
+        string? aFriend;
+        aFriend = Console.ReadLine();
+
+        Console.WriteLine("Hello " + aFriend + ", nice to meet you!");
+        Console.ReadLine();
+    
+        }
+}
