5ad99a83e5e7539dcc69ea9332feb7533ccf7b42
olivia474 <olivia.lengthorn@bp.com>
2023-10-01 15:00:11 +0100

    Adding if, else if and else statements

--- Program.cs
+++ Program.cs
@@ -3,12 +3,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What is your name?");
-        string? aFriend;
-        aFriend = Console.ReadLine();
+        Console.Write("What is your first name?: ");
+        string? firstName = Console.ReadLine();
 
-        Console.WriteLine("Hello " + aFriend + ", nice to meet you!");
-        Console.ReadLine();
-    
+        Console.Write("What is your surname?: ");
+        string? lastName = Console.ReadLine();
+        
+         if(string.IsNullOrWhiteSpace(firstName) && !string.IsNullOrWhiteSpace(lastName)){
+            Console.WriteLine("Hey, looks like you forgot to enter your first name!");
         }
+
+        else if (!string.IsNullOrWhiteSpace(firstName) && string.IsNullOrWhiteSpace(lastName)) {
+            Console.WriteLine("Oops! You didn't enter your last name. Mind filling that in?");
+        }
+
+        else if(string.IsNullOrWhiteSpace(firstName) && string.IsNullOrWhiteSpace(lastName)) {
+            
+            Console.WriteLine("Uh oh! It seems like you forgot to enter both your first name and your last name. Can you please provide them?");
+        }
+        
+        else {
+            Console.WriteLine("Great! Thank you for entering your first name and last name.");
+        }
+
+        Console.ReadLine();
+     
+    }
 }
